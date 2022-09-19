ea49f8278f0f5558b22dd9b6f154e47a39b26d5a
Hrairi Mehdi <49190228+MEHDIHRAIRI@users.noreply.github.com>
2022-09-19 03:40:32 +0200

    blog styling

--- client/src/components/Cards.js
+++ client/src/components/Cards.js
@@ -1,21 +1,47 @@
 import React from "react";
-import { Card } from "antd";
+import "./Cards.css";
 import { useSelector } from "react-redux";
 import LoadingCards from "./LoadingCards";
-const gridStyle = {
-  width: "50%",
-  textAlign: "center",
-};
+
 function Cards() {
   const blogs = useSelector((state) => state.blogs);
   return !blogs.length ? (
     <LoadingCards />
   ) : (
-    <Card style={{ marginTop: "7%" }}>
-      {blogs.map((blog) => (
-        <Card.Grid style={gridStyle}>{blog.title}</Card.Grid>
-      ))}
-    </Card>
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        padding: "10%",
+        backgroundColor: "#d8f3dc",
+      }}
+    >
+      <ol className="articles">
+        {blogs.map((blog) => (
+          <li className="articles__article">
+            <a className="articles__link">
+              <div className="articles__content articles__content--lhs">
+                <h2 className="articles__title">{blog.title}</h2>
+                <div className="articles__footer">
+                  <p>{blog.author}</p>
+                  <time>{blog._id}</time>
+                </div>
+              </div>
+              <div
+                className="articles__content articles__content--rhs"
+                aria-hidden="true"
+              >
+                <h2 className="articles__title">{blog.title}</h2>
+                <div className="articles__footer">
+                  <p>{blog.author}</p>
+                  <time>{blog._id}</time>
+                </div>
+              </div>
+            </a>
+          </li>
+        ))}
+      </ol>
+    </div>
   );
 }
 
--- client/src/pages/Blogs.js
+++ client/src/pages/Blogs.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Banner from "../components/Banner";
+import Cards from "../components/Cards";
 
 function Blogs() {
-  return <Banner />;
+  return (
+    <>
+      <Banner />
+      <Cards />
+    </>
+  );
 }
 
 export default Blogs;
