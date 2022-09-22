d03b7626e1be6c1eeabb98f07128b53981ed87ce
Hrairi Mehdi <49190228+MEHDIHRAIRI@users.noreply.github.com>
2022-09-22 11:02:59 +0200

    add: blog details display

--- client/src/pages/BlogDetails/BlogDetails.js
+++ client/src/pages/BlogDetails/BlogDetails.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
+import Banner from "../Blogs/components/Banner";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import ThumbUpOffAltIcon from "@mui/icons-material/ThumbUpOffAlt";
+import ThumbDownOffAltIcon from "@mui/icons-material/ThumbDownOffAlt";
+import "./blogDetails.css";
 
 function BlogDetails() {
   const { blogId } = useParams();
@@ -10,14 +15,25 @@ function BlogDetails() {
 
   return (
     <div>
+      <Banner />
       {blog ? (
-        <>
-          <p>{blog._id}</p>
-          <p>{blog.title}</p>
-          <p>{blog.content}</p>
-
-          <p>{blog.author}</p>
-        </>
+        <div className="blog">
+          <div className="blogDetails">
+            <h1 className="title">{blog.title}</h1>
+            <p className="author">
+              <PersonOutlineIcon /> author: {blog.author}
+            </p>
+            <h2 className="content">{blog.content}</h2>
+            <div className="rating">
+              <ThumbUpOffAltIcon fontSize="large" /> <h2>8</h2>
+              <ThumbDownOffAltIcon
+                fontSize="large"
+                style={{ marginLeft: "8px" }}
+              />{" "}
+              <h2>4</h2>
+            </div>
+          </div>
+        </div>
       ) : (
         ""
       )}
